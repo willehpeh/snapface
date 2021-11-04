4acac3567f615e55120115d20b99d59bf0488215
willehpeh <will_alexander@me.com>
2021-11-04 15:59:20 +0100

    Data added

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {}
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+
+  ngOnInit() {
+    this.title = 'Archibald';
+    this.description = 'Mon meilleur ami depuis tout petit !';
+    this.createdDate = new Date();
+    this.snaps = 6;
+  }
+}

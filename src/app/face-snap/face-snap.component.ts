ae6eeb98a743e551b42ed9a03cc057141e4906cf
willehpeh <will_alexander@me.com>
2021-11-04 16:46:20 +0100

    Attribute binding done

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -10,11 +10,13 @@ export class FaceSnapComponent implements OnInit {
   description!: string;
   createdDate!: Date;
   snaps!: number;
+  imageUrl!: string;
 
   ngOnInit() {
     this.title = 'Archibald';
     this.description = 'Mon meilleur ami depuis tout petit !';
     this.createdDate = new Date();
     this.snaps = 6;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
   }
 }

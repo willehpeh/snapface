0fdeee63bbbd28ecf43e84db29d47f3f71be646f
willehpeh <will_alexander@me.com>
2021-11-04 12:30:41 +0100

    End of chapter

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent {}

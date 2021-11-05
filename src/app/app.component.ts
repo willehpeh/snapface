17edc8506132a53bb54d6efbae315603e5bba80c
willehpeh <will_alexander@me.com>
2021-11-05 21:28:48 +0100

    Optional property added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,14 +17,16 @@ export class AppComponent implements OnInit {
       description: 'Mon meilleur ami depuis tout petit !',
       imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
       createdDate: new Date(),
-      snaps: 0
+      snaps: 0,
+      location: 'Paris'
     };
     this.myOtherSnap = {
       title: 'Three Rock Mountain',
       description: 'Un endroit magnifique pour les randonnées.',
       imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
       createdDate: new Date(),
-      snaps: 0
+      snaps: 0,
+      location: 'la montagne'
     };
     this.myLastSnap = {
       title: 'Un bon repas',
--- src/app/models/face-snap.model.ts
+++ src/app/models/face-snap.model.ts
@@ -1,8 +1,8 @@
 export class FaceSnap {
-  constructor(public title: string,
-              public description: string,
-              public imageUrl: string,
-              public createdDate: Date,
-              public snaps: number) {
-  }
+  title!: string;
+  description!: string;
+  imageUrl!: string;
+  createdDate!: Date;
+  snaps!: number;
+  location?: string;
 }

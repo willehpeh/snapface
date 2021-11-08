abd4c5b5502c765d33b4d8c908eeb31bf5d90f36
willehpeh <will_alexander@me.com>
2021-11-08 13:45:01 +0100

    chapter start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,33 +7,57 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+
+  faceSnaps!: FaceSnap[];
 
   ngOnInit() {
-    this.mySnap = {
-      title: 'Archibald',
-      description: 'Mon meilleur ami depuis tout petit !',
-      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      createdDate: new Date(),
-      snaps: 0,
-      location: 'Paris'
-    };
-    this.myOtherSnap = {
-      title: 'Three Rock Mountain',
-      description: 'Un endroit magnifique pour les randonnées.',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-      createdDate: new Date(),
-      snaps: 6,
-      location: 'la montagne'
-    };
-    this.myLastSnap = {
-      title: 'Un bon repas',
-      description: 'Mmmh que c\'est bon !',
-      imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-      createdDate: new Date(),
-      snaps: 0
-    };
+    this.faceSnaps = [
+      {
+        title: 'Archibald',
+        description: 'Mon meilleur ami depuis tout petit !',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        location: 'Paris'
+      },
+      {
+        title: 'Three Rock Mountain',
+        description: 'Un endroit magnifique pour les randonnées.',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+        createdDate: new Date(),
+        snaps: 6,
+        location: 'la montagne'
+      },
+      {
+        title: 'Un bon repas',
+        description: 'Mmmh que c\'est bon !',
+        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        createdDate: new Date(),
+        snaps: 0
+      },
+      {
+        title: 'Archibald',
+        description: 'Mon meilleur ami depuis tout petit !',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        location: 'Paris'
+      },
+      {
+        title: 'Three Rock Mountain',
+        description: 'Un endroit magnifique pour les randonnées.',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+        createdDate: new Date(),
+        snaps: 6,
+        location: 'la montagne'
+      },
+      {
+        title: 'Un bon repas',
+        description: 'Mmmh que c\'est bon !',
+        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        createdDate: new Date(),
+        snaps: 0
+      }
+    ];
   }
 }

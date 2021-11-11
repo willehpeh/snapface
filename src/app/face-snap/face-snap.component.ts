2bafb54caf9fc47b4b72a6a8e3af307197777d7b
willehpeh <will_alexander@me.com>
2021-11-11 12:51:10 +0100

    P5CH2C-start

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
+import { FaceSnapsService } from '../services/face-snaps.service';
 
 @Component({
   selector: 'app-face-snap',
@@ -10,13 +11,15 @@ export class FaceSnapComponent implements OnInit {
   @Input() faceSnap!: FaceSnap;
   buttonText!: string;
 
+  constructor(private faceSnapsService: FaceSnapsService) {}
+
   ngOnInit() {
     this.buttonText = 'Oh Snap!';
   }
 
   onSnap() {
     if (this.buttonText === 'Oh Snap!') {
-      this.faceSnap.snaps++;
+      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id);
       this.buttonText = 'Oops, unSnap!';
     } else {
       this.faceSnap.snaps--;
--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -7,6 +7,7 @@ import { FaceSnap } from '../models/face-snap.model';
 export class FaceSnapsService {
   faceSnaps: FaceSnap[] = [
     {
+      id: 1,
       title: 'Archibald',
       description: 'Mon meilleur ami depuis tout petit !',
       imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
@@ -15,6 +16,7 @@ export class FaceSnapsService {
       location: 'Paris'
     },
     {
+      id: 2,
       title: 'Three Rock Mountain',
       description: 'Un endroit magnifique pour les randonnées.',
       imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
@@ -23,6 +25,7 @@ export class FaceSnapsService {
       location: 'la montagne'
     },
     {
+      id: 3,
       title: 'Un bon repas',
       description: 'Mmmh que c\'est bon !',
       imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
@@ -30,6 +33,7 @@ export class FaceSnapsService {
       snaps: 156
     },
     {
+      id: 4,
       title: 'Archibald',
       description: 'Mon meilleur ami depuis tout petit !',
       imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
@@ -38,6 +42,7 @@ export class FaceSnapsService {
       location: 'Paris'
     },
     {
+      id: 5,
       title: 'Three Rock Mountain',
       description: 'Un endroit magnifique pour les randonnées.',
       imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
@@ -46,6 +51,7 @@ export class FaceSnapsService {
       location: 'la montagne'
     },
     {
+      id: 6,
       title: 'Un bon repas',
       description: 'Mmmh que c\'est bon !',
       imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
@@ -56,5 +62,23 @@ export class FaceSnapsService {
 
   getAllFaceSnaps(): FaceSnap[] {
     return this.faceSnaps;
+  }
+
+  snapFaceSnapById(faceSnapId: number): void {
+    const faceSnap = this.faceSnaps.find(faceSnap => faceSnap.id === faceSnapId);
+    if (faceSnap) {
+      faceSnap.snaps++;
+    } else {
+      throw new Error('FaceSnap not found!');
+    }
+  }
+
+  unsnapFaceSnapById(faceSnapId: number): void {
+    const faceSnap = this.faceSnaps.find(faceSnap => faceSnap.id === faceSnapId);
+    if (faceSnap) {
+      faceSnap.snaps--;
+    } else {
+      throw new Error('FaceSnap not found!');
+    }
   }
 }
